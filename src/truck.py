2558d1f0b3c93a19c3e10cfd27d6f9490e9df4b9
Drum Holliday <107374333+PurpleBird3@users.noreply.github.com>
2026-04-27 19:20:35 -0500

    Added method to truck.py to remove a package from the truck after it is delivered.

--- src/truck.py
+++ src/truck.py
@@ -14,3 +14,7 @@ class Truck:
             return True
         else:
             return False
+    # Remove a package from the truck after it's delivered and keep track of remaining deliveries.
+    def remove_package(self, package_id):
+        if package_id in self.packages:
+            self.packages.remove(package_id)
